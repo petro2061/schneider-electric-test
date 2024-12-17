a206ffcb468802f05fca2300c20bb6e1c676258d
petro feniak <petro.fenyak@gmail.com>
2024-12-17 22:48:58 +0200

    simulation of unit operation in main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
 public class Main {
+    private static final double START_LEVEL = 25.0;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TankControlSystem system = new TankControlSystem(START_LEVEL);
+
+        system.openFillValve();
+        for (int i = 0; i < 10; i++) {
+            system.controlSystem();
+            system.simulateStep(8.0);
+        }
+
+        system.setDrainBottom(true);
+        for (int i = 0; i < 5; i++) {
+            system.controlSystem();
+            system.simulateStep(10.0);
+        }
+
+        system.closeFillValve();
+        system.controlSystem();
     }
 }
